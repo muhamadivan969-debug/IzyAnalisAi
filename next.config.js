501e314c8be8ee5e08a7cbae924aa6d89c4f6872
IzyAnalisAi <muhamadivan969@gmail.com>
2026-08-07 06:17:14 +0700

    Enable SWC minification and compression options

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
+  generateEtags: false,
 };
 
 export default nextConfig;
