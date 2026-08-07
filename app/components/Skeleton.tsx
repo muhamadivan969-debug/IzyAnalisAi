2a91c4f18e79fac9c5091ff8dd16d08cae131c6a
IzyAnalisAi <muhamadivan969@gmail.com>
2026-08-07 09:34:28 +0700

    Update Skeleton component with animation styles

--- app/components/Skeleton.tsx
+++ app/components/Skeleton.tsx
@@ -1,3 +1,3 @@
 export default function Skeleton({ className = '' }: { className?: string }) {
-  return <div className={`shimmer ${className}`} />;
+  return <div className={`animate-pulse bg-[#162035] rounded-lg ${className}`} />;
 }
