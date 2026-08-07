1fe3aa0a22e7b3ff39107f3436f8f7c6d2305cc5
IzyAnalisAi <muhamadivan969@gmail.com>
2026-08-07 09:35:31 +0700

    Change PulseDot color to green

--- app/components/PulseDot.tsx
+++ app/components/PulseDot.tsx
@@ -1,8 +1,8 @@
 export default function PulseDot() {
   return (
     <span className="relative flex h-2 w-2">
-      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#00d26a] opacity-75"></span>
-      <span className="relative inline-flex rounded-full h-2 w-2 bg-[#00d26a]"></span>
+      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-75"></span>
+      <span className="relative inline-flex rounded-full h-2 w-2 bg-green"></span>
     </span>
   );
 }
